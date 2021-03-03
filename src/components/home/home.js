3b21efa436f71656c07815eff4cbe6d1cd325d88
alexpritt <alexandercpritt@gmail.com>
2021-03-03 14:41:22 -0500

    building homepage, styling adjustments

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,3 +1,6 @@
+// TODO: Add html components to navigation buttons
+// TODO: Setup routing to first go through bootup when requesting site
+
 import '../../App.css';
 import TypeIt from "typeit-react";
 
@@ -7,8 +10,20 @@ function Home() {
         <TypeIt
             getBeforeInit={instance => {
                 instance
-                .options({speed:20})
+                .options({speed:30})
                 .type("OVERMONITORING ADDRESS MATRIX")
+                .break()
+                .break()
+                .options({speed:30})
+                .type("|FILES", {delay:150})
+                .break()
+                .break()
+                .options({speed:30})
+                .type("|SYSTEM", {delay:150})
+                .break()
+                .break()
+                .options({speed:30})
+                .type("|NOTEPAD", {delay:150})
                 return instance;
             }}
         />
